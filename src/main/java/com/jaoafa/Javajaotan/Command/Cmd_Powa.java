e2d7b432dd3851d13f80be3b951ef6ac07155414
Tomachi[ICHIGO] <lego@aol.jp>
2019-07-12 20:06:51 +0900

    change: powa

--- src/main/java/com/jaoafa/Javajaotan/Command/Cmd_Powa.java
+++ src/main/java/com/jaoafa/Javajaotan/Command/Cmd_Powa.java
@@ -1,14 +1,27 @@
 package com.jaoafa.Javajaotan.Command;
 
+import com.jaoafa.Javajaotan.CommandPremise;
+
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.IUser;
 
-public class Cmd_Powa {
-	public static void onCommand(IDiscordClient client, IGuild guild, IChannel channel, IUser author, IMessage message, String[] args){
+public class Cmd_Powa implements CommandPremise {
+	@Override
+	public void onCommand(IDiscordClient client, IGuild guild, IChannel channel, IUser author, IMessage message, String[] args){
 		// powa command
 		channel.sendMessage("ポわ～～～～～～～ｗｗｗｗ！！！ｗ！ｗｗ！ｗ！ｗ");
 	}
+
+    @Override
+    public String getDescription() {
+        return "「ポわ～～～～～～～ｗｗｗｗ！！！ｗ！ｗｗ！ｗ！ｗ」を発言されたチャンネルに投稿します。";
+    }
+
+    @Override
+    public String getUsage() {
+        return "/powa";
+    }
 }
