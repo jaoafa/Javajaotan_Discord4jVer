63c3ec74d5e2e51dce34b9a658d456a63751fe52
Tomachi[ICHIGO] <lego@aol.jp>
2019-07-14 17:35:53 +0900

    fix: blookupコマンド修正

--- src/main/java/com/jaoafa/Javajaotan/Command/Cmd_Blookup.java
+++ src/main/java/com/jaoafa/Javajaotan/Command/Cmd_Blookup.java
@@ -8,7 +8,7 @@ import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.IUser;
 
-public class Cmd_blookup implements CommandPremise {
+public class Cmd_Blookup implements CommandPremise {
 	@Override
 	public void onCommand(IDiscordClient client, IGuild guild, IChannel channel, IUser author, IMessage message, String[] args){
 		if(args.length == 0){
