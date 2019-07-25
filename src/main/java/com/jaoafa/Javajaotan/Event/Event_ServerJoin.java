9eb5b31fd3af614f3cc00805532462abcf72c978
Tomachi[MINT] <lego@aol.jp>
2019-07-26 02:18:26 +0900

    add: 参加時にgeneralにもメッセージを送るように

--- src/main/java/com/jaoafa/Javajaotan/Event/Event_ServerJoin.java
+++ src/main/java/com/jaoafa/Javajaotan/Event/Event_ServerJoin.java
@@ -12,8 +12,11 @@ public class Event_ServerJoin {
 			return; // jMS Gamers Clubのみ
 		}
 		IUser user = event.getUser();
-		IChannel channel = event.getGuild().getChannelByID(603841992404893707L);
-		channel.sendMessage(":man_dancing:<@" + user.getStringID() + ">(#" + user.getDiscriminator() + ")さん、jao Minecraft Server Discordにようこそ。\n"
+
+		IChannel general = event.getGuild().getChannelByID(597419057251090443L);
+		general.sendMessage(":man_dancing:<@" + user.getStringID() + ">(#" + user.getDiscriminator() + ")さんがjMS Gamers Clubに参加しました。");
+		IChannel greeting = event.getGuild().getChannelByID(603841992404893707L);
+		greeting.sendMessage(":man_dancing:<@" + user.getStringID() + ">(#" + user.getDiscriminator() + ")さん、jao Minecraft Server Discordにようこそ。\n"
 			+ "運営方針により、参加から10分以内に発言がない場合システムによって自動的にキックされます。<#603841992404893707>チャンネルで「jao」「afa」とあいさつしてみましょう。");
 	}
 }
