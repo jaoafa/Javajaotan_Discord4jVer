e164adb02cc134c8777487d29e35ed4ffd442bb2
Tomachi[ICHIGO] <lego@aol.jp>
2019-09-26 23:43:16 +0900

    change: todoでのjaotanからのメッセ削除

--- src/main/java/com/jaoafa/Javajaotan/Event/Event_TodoCheck.java
+++ src/main/java/com/jaoafa/Javajaotan/Event/Event_TodoCheck.java
@@ -3,6 +3,7 @@ package com.jaoafa.Javajaotan.Event;
 import com.jaoafa.Javajaotan.Main;
 
 import sx.blah.discord.api.events.EventSubscriber;
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageSendEvent;
 import sx.blah.discord.handle.impl.events.guild.channel.message.reaction.ReactionAddEvent;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IMessage;
@@ -25,7 +26,7 @@ public class Event_TodoCheck {
 			message = channel.fetchMessage(message.getLongID());
 		}
 		IReaction white_check_mark = message.getReactionByUnicode("\u2705");
-		if (white_check_mark.getCount() == 0) {
+		if (white_check_mark == null || white_check_mark.getCount() == 0) {
 			return;
 		}
 		RequestBuffer.request(() -> {
@@ -36,5 +37,24 @@ public class Event_TodoCheck {
 				Main.DiscordExceptionError(getClass(), channel, discordexception);
 			}
 		});
+	}
+
+	@EventSubscriber
+	public void onMessageSendEvent(MessageSendEvent event) {
+		IChannel channel = event.getChannel();
+		IMessage message = event.getMessage();
+		if (!message.isSystemMessage()) {
+			return;
+		}
+		// #todo | #develop_todo
+		if (channel.getLongID() != 597424023621599232L && channel.getLongID() != 626727474922913792L) {
+			return;
+		}
+		// jaotan
+		if (event.getAuthor().getLongID() != 222018383556771840L) {
+			// 発生しないはず
+			return;
+		}
+		message.delete();
 	}
 }
