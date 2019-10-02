b5ae4eee100da74401ae8d9b09b5e8853cc8e076
Tomachi[ICHIGO] <lego@aol.jp>
2019-10-02 23:58:46 +0900

    change: greetingでafa発言時の反応処理追加

--- src/main/java/com/jaoafa/Javajaotan/Channel/Channel_603841992404893707.java
+++ src/main/java/com/jaoafa/Javajaotan/Channel/Channel_603841992404893707.java
@@ -1,5 +1,8 @@
 package com.jaoafa.Javajaotan.Channel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.jaoafa.Javajaotan.ChannelPremise;
 import com.jaoafa.Javajaotan.Main;
 
@@ -15,6 +18,8 @@ import sx.blah.discord.util.RequestBuffer;
 
 public class Channel_603841992404893707 implements ChannelPremise {
 	// #greeting 603841992404893707
+	private static List<Long> jaoPlayers = new ArrayList<Long>();
+
 	@Override
 	public void run(IDiscordClient client, IGuild guild, IChannel channel, IUser author, IMessage message,
 			boolean edited) {
@@ -38,11 +43,43 @@ public class Channel_603841992404893707 implements ChannelPremise {
 		}
 		if (message.getContent().equals("jao")) {
 			if (!author.hasRole(role)) {
-				author.addRole(role);
-				message.addReaction(ReactionEmoji.of("\u2B55")); // o
+				RequestBuffer.request(() -> {
+					try {
+						author.addRole(role);
+						message.addReaction(ReactionEmoji.of("\u2B55")); // o
+					} catch (DiscordException discordexception) {
+						Main.DiscordExceptionError(getClass(), channel, discordexception);
+					}
+				});
+				jaoPlayers.add(author.getLongID());
 			} else {
-				message.addReaction(ReactionEmoji.of("\u274C")); // x
+				RequestBuffer.request(() -> {
+					try {
+						message.addReaction(ReactionEmoji.of("\u274C")); // x
+					} catch (DiscordException discordexception) {
+						Main.DiscordExceptionError(getClass(), channel, discordexception);
+					}
+				});
 			}
+		} else if (message.getContent().equals("afa")) {
+			if (!jaoPlayers.contains(author.getLongID())) {
+				RequestBuffer.request(() -> {
+					try {
+						message.addReaction(ReactionEmoji.of("\u274C")); // x
+					} catch (DiscordException discordexception) {
+						Main.DiscordExceptionError(getClass(), channel, discordexception);
+					}
+				});
+				return;
+			}
+			RequestBuffer.request(() -> {
+				try {
+					message.reply("あいさつしていただきありがとうございます！これにより、多くのチャンネルを閲覧できるようになりました。\n" +
+							"このあとは<#597419057251090443>などで「`/link`」を実行(投稿)して、MinecraftアカウントとDiscordアカウントを連携しましょう！");
+				} catch (DiscordException discordexception) {
+					Main.DiscordExceptionError(getClass(), channel, discordexception);
+				}
+			});
 		}
 	}
 
